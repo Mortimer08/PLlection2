8e9a6ae1ccb356dcdc2acf30426e4f02034a2005
Ilya Makarov <mortimer08@gmail.com>
2022-10-16 22:28:02 +0500

    Example03 Complete

--- Example03/Program.cs
+++ Example03/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = { 111, 22, 39, 7, 34, 25, 66, 7, 38, 93 };
+
+int n = array.Length; // Длина массива
+int find = 7;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
