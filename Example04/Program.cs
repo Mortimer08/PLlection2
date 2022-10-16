a960902421547e986154ceccac64e411e1065d16
Ilya Makarov <mortimer08@gmail.com>
2022-10-16 23:50:03 +0500

    Example04 Complet2

--- Example04/Program.cs
+++ Example04/Program.cs
@@ -1,4 +1,4 @@
-﻿void FillArray(int[] collection)
+﻿void FillArray(int[] collection) // void - метод без возврата данных
 {
     int length = collection.Length;
     int index = 0;
@@ -11,15 +11,40 @@
 
 void PrintArray(int[] col)
 {
-    int count = col.Length;
-    int position = 0;
+    int count = col.Length;      // Length - метод, возвращающий длину массива
+    int position = 0;           // Для объявления переменной нужно присваивать ей значение
     while (position < count)
     {
         Console.WriteLine(col[position]);
         position++;
     }
 }
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int[10];
 
 FillArray(array);
+array[4] = 4;
+array[7] = 4;
 PrintArray(array);
+
+Console.WriteLine("");
+int pos = IndexOf(array, 44);
+Console.WriteLine(pos);
